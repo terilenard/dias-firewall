40b198594b4ee8c9f41124e16ce0cdacfaea9562
Teri Lenard <teri.lenard@umfst.ro>
2022-05-11 15:24:01 +0300

    Added license and authors in python source files

--- Utils/pyclient_mqtt.py
+++ Utils/pyclient_mqtt.py
@@ -1,3 +1,14 @@
+"""
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT>.
+
+Developed by NISLAB - Network and Information Security Laboratory
+at George Emil Palade University of Medicine, Pharmacy, Science and
+Technology of Târgu Mureş <https://nislab.umfst.ro/>
+
+Contributors: Teri Lenard
+"""
+
 #!/usr/bin/env python3 
 import time
 import signal
